52455e22e0269d5bf193835d364be3f830216463
CrispyDrone <mybluewaterfall@hotmail.com>
2019-03-19 23:23:28 +0100

    WIP: ListView, searching and filtering

---
+++ Main/ReplayParser.ReplaySorter.UI/Converters/MapToFileNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ReplayParser.ReplaySorter.UI.Converters
+{
+    // alternatively complete the parsing of the map, and somehow render a preview from it...
+    public class MapToFileNameConverter : IValueConverter
+    {
+        // structure to map strings that match regex to a constant string, which is the map name that will be mapped to an image
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var mapName = value as string;
+            if (string.IsNullOrWhiteSpace(mapName))
+                return string.Empty;
+
+            return $"/images/maps/fighting_spirit.jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Main/ReplayParser.ReplaySorter.UI/Converters/PlayerToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using ReplayParser.Interfaces;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ReplayParser.ReplaySorter.UI.Converters
+{
+    public class PlayerToVisibilityConverter : IMultiValueConverter
+    {
+
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var player = values[0] as IPlayer;
+            if (player == null)
+                return Visibility.Hidden;
+
+            var file = values[1] as IO.File<IReplay>;
+            if (file == null)
+                return Visibility.Hidden;
+
+            if (file.Content.Winner.Contains(player))
+                return Visibility.Visible;
+
+            return Visibility.Hidden;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
