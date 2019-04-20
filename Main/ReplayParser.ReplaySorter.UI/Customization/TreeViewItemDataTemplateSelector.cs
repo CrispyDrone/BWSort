451cc79c9f5c5443c10688f5b03f2bf2bad18786
CrispyDrone <mybluewaterfall@hotmail.com>
2019-04-21 00:51:08 +0200

    TreeView styling

---
+++ Main/ReplayParser.ReplaySorter.UI/Customization/TreeViewItemDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using ReplayParser.ReplaySorter.Sorting.SortResult;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReplayParser.ReplaySorter.UI.Customization
+{
+    public class TreeViewItemDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DirectoryTemplate { get; set; }
+        public DataTemplate FileTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item != null && item is DirectoryFileTreeNode)
+            {
+                var directoryFileTreeNode = item as DirectoryFileTreeNode;
+
+                if (directoryFileTreeNode.IsDirectory)
+                    return DirectoryTemplate;
+
+                return FileTemplate;
+            }
+
+            return null;
+        }
+    }
+}
